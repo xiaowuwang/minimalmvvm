e9930bd4114e1f0d829aefcbb6bdaa7ee156d855
steven <stevenwang@hotmail.co.nz.co.nz>
2017-03-13 21:51:03 +1300

    REVERT

---
+++ commandBindings/RelayCommand.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace commandBindings
+{
+    #region CommandBase
+    public class RelayCommand : ICommand
+    {
+        #region private fields
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+        #endregion
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this.canExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if (this.canExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null ? true : this.canExecute();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        #region private fields
+        private readonly Action<T> execute;
+        private readonly Predicate<T> canExecute;
+        #endregion
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this.canExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if (this.canExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute((T)parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (parameter is T)
+            {
+                return this.canExecute == null ? true : this.canExecute((T)parameter);
+            }
+
+#if DEBUG
+            if (parameter == null)
+            {
+                // Debug.WriteLine("RelayCommand: parameter is null");
+            }
+            else
+            {
+                Debug.WriteLine("RelayCommand: the type of the parameter does not match current type " +
+                    parameter.GetType() + " excepted type " + typeof(T));
+            }
+#endif
+
+            return true;
+        }
+    }
+    #endregion
+}
